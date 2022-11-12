a0ef94b7d54335a2d7e3e9047531908da531e98c
Thiago Nascimento <nascimentoliveira@gmail.com>
2022-11-12 15:59:52 -0300

    feat: status route (post)

--- src/index.js
+++ src/index.js
@@ -152,6 +152,30 @@ app.get('/messages', async (req, res) => {
   }
 });
 
+/* Status Route */
+app.post('/status', async (req, res) => {
+  const user = req.headers.user;
+
+  if (!user) {
+    return res.status(422).send({ message: 'Unexpected header format. Field "user" expected.' });
+  }
+
+  try {
+    const userExists = await participants.findOne({ name: user });
+
+    if (!userExists)
+      return res.status(404).send({ message: 'User not found' });
+
+    await participants.updateOne({_id: userExists._id}, { $set: { lastStatus: Date.now()}});
+
+    res.status(200).send({ message: 'Updated status' });
+
+  } catch (err) {
+    console.error('An error has occurred:', err);
+    res.status(500).send({ message: 'An error has occurred', error: err });
+  }
+});
+
 /* Server listen */
 app.listen(PORT, function (err) {
   if (err) console.log('Failed to start the server -', err);
